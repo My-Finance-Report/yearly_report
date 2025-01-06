88588d610059b0711334f823d7f1e75d5bf8520a
Matt Carroll <mcarroll1220@gmail.com>
2025-01-06 12:59:20 -0500

    begin migrating by making models

---
+++ src/Models.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+import Database.Persist.TH
+
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+
+TransactionSource
+    name Text
+    deriving Show Eq
+
+Category
+    name Text
+    sourceId TransactionSourceId
+    UniqueCategory name sourceId
+    deriving Show Eq
+
+Transaction
+    description Text
+    categoryId CategoryId
+    dateOfTransaction Text
+    amount Double
+    transactionSourceId TransactionSourceId
+    kind Text
+    filename Text Maybe
+    deriving Show Eq
+
+ProcessedFile
+    filename Text
+    UniqueProcessedFile filename
+    deriving Show Eq
+
+UploadedPdf
+    filename Text
+    rawContent Text
+    uploadTime Text
+    deriving Show Eq
+
+UploadConfiguration
+    filenameRegex Text Maybe
+    startKeyword Text Maybe
+    endKeyword Text Maybe
+    transactionSourceId TransactionSourceId
+    UniqueUploadConfiguration transactionSourceId
+    deriving Show Eq
+
+SankeyConfig
+    name Text
+    UniqueSankeyConfig name
+    deriving Show Eq
+
+SankeyInput
+    configId SankeyConfigId
+    sourceId TransactionSourceId
+    categoryId CategoryId
+    deriving Show Eq
+
+SankeyLinkage
+    configId SankeyConfigId
+    sourceId TransactionSourceId
+    categoryId CategoryId
+    targetSourceId TransactionSourceId
+    deriving Show Eq
+|]
