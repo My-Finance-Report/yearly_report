9a7a2e06c6d3142a5fb14e516f05ca00097dc50f
Matt Carroll <mcarroll1220@gmail.com>
2025-01-16 18:44:49 -0500

    testing

--- src/HtmlGenerators/LandingPage.hs
+++ src/HtmlGenerators/LandingPage.hs
@@ -54,27 +54,26 @@ renderLandingPage =
       H.div ! A.class_ "banner" $ do
         H.h1 "My Financé"
         H.h2 "Because you should love your finances"
-        H.h3 "Simple personal finance for just $2/month or $20/year."
+        H.h2 ! A.class_ "subtag" $ "Simple personal finance for just $2/month or $20/year."
 
       H.div ! A.class_ "features-section" $ do
-        H.div ! A.class_ "cards-container" $ do
-          featureCard "Process Statements" "Upload a statement and we do the rest"
-          featureCard "Categorize expenses automatically" "Create custom categorizes and group transactions"
-          featureCard "Generate detailed reports" "Understand where your money is actually going!"
+        H.div ! A.class_ "landing-page-cards-container" $ do
+          featureCard "Upload Transactions" "Automatically pull transactions from your bank statements"
+          featureCard "Categorize Expenses" "Automatically categorize all your expenses"
+          featureCard "See The Money Flow" "Understand where your money is actually going!"
 
-      -- Call-to-action Section
-      H.div ! A.class_ "cta-section" $ do
-        H.h3 "Play in the sandbox"
-        H.p "or straight to sign up if our sales pitch sold you"
-        H.div ! A.class_ "button-group" $ do
-          H.button
-            ! A.class_ "explore-btn"
-            ! A.onclick "window.location.href='/demo-account'"
-            $ "Explore"
-          H.button
-            ! A.class_ "login-btn"
-            ! A.onclick "window.location.href='/login'"
-            $ "Log in"
+        H.div ! A.class_ "cta-section" $ do
+          H.h3 "Explore the sandbox"
+          H.p "or  to sign up if our sales pitch sold you"
+          H.div ! A.class_ "button-group" $ do
+            H.button
+              ! A.class_ "explore-btn"
+              ! A.onclick "window.location.href='/demo-account'"
+              $ "Explore"
+            H.button
+              ! A.class_ "login-btn"
+              ! A.onclick "window.location.href='/login'"
+              $ "Sign up"
 
 
 -- Helper function to generate a feature card
