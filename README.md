05b8eee3758ccb2e2abd0721b0846f6973fc9a09
Matt Carroll <mcarroll1220@gmail.com>
2025-01-19 13:25:08 -0500

    improve the style on the upload page

--- README.md
+++ README.md
@@ -25,17 +25,16 @@ and has evolved a bit to be a [simple webapp](https://myfinancereport.com/).
 
 this is mainly a project through which I will learn more about haskell, but I expect the code to be bad.
 
-# Launch blocking
+# Blocking
 
-- make the column charts make sense
-- sort is wrong on the barchart x axis
-- the configuration page is just wacky
-- sankey config has configability somewhere
+file is not processed but also not listed on the processed files page
+-> maybe just remove "processed files"
+
+delete button in the table doesnt work
 
 # Mom feedback
 
 - make it clear when they start the onboarding that they need their bank / cc statements
-- sankey needs to fix within its container
 - First page need to know you need statements
   -> import to be able to name credit cards
   -> the first page spells out all the things you need to do
@@ -46,6 +45,10 @@ this is mainly a project through which I will learn more about haskell, but I ex
 
 # Not Blocking
 
+- it seems like the text on mobile just needs to be bigger
+- make the column charts make sense
+- the configuration page is just wacky
+- sankey config has configability somewhere
 - there is some redirect bug in the onboarding flow
 - filter by year when you look at dashboard
 - clicking on charts pops tables would be nice
@@ -59,9 +62,10 @@ this is mainly a project through which I will learn more about haskell, but I ex
 - refactor Main.hs routes
 - refresh on the page keeps you in the same view
 - delete transactions
+- the data model should likely differentiate between cards and accounts and keep it at a type level
+- use this to inform what presets we show, and also which things we want in which charts
 
 # Bad UI
 
 - password dont match in wrong modal
 - let people know something is happening async
-- default to categories being added
--- src/HtmlGenerators/UploadPage.hs
+++ src/HtmlGenerators/UploadPage.hs
@@ -9,24 +9,51 @@ import Text.Blaze.Html5.Attributes as A
 
 makeToolBar :: Html
 makeToolBar =
-  H.div ! A.class_ "page-header" $ do
-    H.div ! A.class_ "upload-section" $ do
-      H.form
-        ! A.action "/upload"
-        ! A.method "post"
-        ! A.class_ "upload_form"
-        ! A.enctype "multipart/form-data"
-        $ do
-          H.input
-            ! A.type_ "file"
-            ! A.name "pdfFile"
-            ! A.accept "application/pdf"
-            ! A.id "pdfFileInput"
-          H.button
-            ! A.type_ "submit"
-            ! A.class_ "btn upload-btn"
-            ! A.id "uploadButton"
-            $ "Add Transactions"
+  H.div ! A.class_ "w-full max-w-3xl mx-auto text-center" $ do
+    H.script ! A.type_ "text/javascript" ! A.src "/upload.js" $ mempty
+
+    -- Page Title
+    H.h1 ! A.class_ "text-4xl font-bold text-primary mb-4" $ "Upload Transactions"
+    H.p ! A.class_ "text-gray-600 text-lg mb-6" $ "Drag and drop your bank statement PDFs or click to upload."
+
+    -- Upload Form
+    H.form
+      ! A.action "/upload"
+      ! A.method "post"
+      ! A.enctype "multipart/form-data"
+      ! A.class_ "flex flex-col items-center gap-4"
+      $ do
+        -- Upload Section (Dropzone)
+        H.div
+          ! A.class_ "bg-white min-w-96 border-2 border-dashed border-primary rounded-lg p-6 shadow-md flex flex-col items-center cursor-pointer hover:bg-gray-100 transition"
+          ! A.id "dropzone"
+          ! A.ondragover "handleDragOver(event)"
+          ! A.ondrop "handleFileDrop(event)"
+          ! A.onclick "triggerFileInput()"
+          $ do
+            -- Dropzone Instruction
+            H.p ! A.class_ "text-gray-700 font-medium mb-2" $ "Click or Drag files here"
+
+            -- File Input (Hidden)
+            H.input
+              ! A.type_ "file"
+              ! A.name "pdfFiles"
+              ! A.id "pdfFileInput"
+              ! A.class_ "hidden"
+              ! A.accept "application/pdf"
+              ! A.multiple "multiple"
+              ! A.onchange "updateFileList()"
+
+            -- File Preview List
+            H.ul ! A.id "fileList" ! A.class_ "text-gray-700 text-sm mt-2" $ ""
+
+        -- Upload Button
+        H.button
+          ! A.type_ "submit"
+          ! A.class_ "primary-button mt-4"
+          ! A.id "uploadButton"
+          ! A.disabled "disabled"
+          $ "Upload Files"
 
 renderUploadPage :: Entity User -> Html
 renderUploadPage user =
