5c438ba6708991ceb3b86226c9d1242510c4e3f5
Chris Spalding <cspalding93@gmail.com>
2025-04-04 16:44:36 -0700

    maybe fix tags

--- fullstack/backend/build.sh
+++ fullstack/backend/build.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-docker build --platform linux/amd64 -t finance-backend:${IMAGE_TAG} -f Dockerfile .
+docker build --platform linux/amd64 -t finance-backend:${IMAGE_TAG} -t finance-backend:latest -f Dockerfile .
