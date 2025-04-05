e267b832c8d5888122ecd4abc3167a04de8236e9
Chris Spalding <cspalding93@gmail.com>
2025-04-04 17:14:14 -0700

    debug build.sh

--- fullstack/frontend/build.sh
+++ fullstack/frontend/build.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 set -euo pipefail
-
-docker build --platform linux/amd64 --build-arg VITE_API_URL=$VITE_API_URL \
+pwd && docker build --platform linux/amd64 --build-arg VITE_API_URL=$VITE_API_URL \
   -t $ECR_REGISTRY/$ECR_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG \
   -t $ECR_REGISTRY/$ECR_REPOSITORY/$IMAGE_NAME:latest \
   -f Dockerfile .
