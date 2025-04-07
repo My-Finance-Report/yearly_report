6b1ce5c3d7f92961589ada4f90068e50d4169653
Matt Carroll <mcarroll1220@gmail.com>
2025-04-07 06:31:04 -0400

    update the faq

--- fullstack/frontend/src/routes/_layout/faq.tsx
+++ fullstack/frontend/src/routes/_layout/faq.tsx
@@ -18,9 +18,7 @@ export function FAQPage() {
             Is there a way to track net worth?
           </Heading>
           <Text>
-            No, this tool mainly aims to capture the flow of money. 
-            You should use it to determine how much you spend on groceries 
-            each month, for example.
+            Not yet, but soon. With the integration of Plaid, we have started building net worth tracking.
           </Text>
         </Box>
 
@@ -29,20 +27,34 @@ export function FAQPage() {
             Do you offer an integration with bank accounts?
           </Heading>
           <Text>
-            Yes, we have an integration with Plaid. Reach out to me{" "}
-            <Link to="/contact" style={{ textDecoration: "underline", color: "blue" }}>
-              here
-            </Link>
-            {" "}and I can help you get set up.
+            Yes, we integrate with Plaid. This should allow most major (and even minor) banks to be connected.
+            Let me know if you don't see your bank as an option. 
           </Text>
         </Box>
 
         <Box>
           <Heading as="h3" size="md">
-            Why should I trust you?
+            Is it safe to link my accounts?
           </Heading>
           <Text>
-            You don’t have to. The app is open source (
+            Yes, we use Plaid's secure authentication process to link your accounts. We cannot generate transfers from your accounts -- just read transactions from them.
+          </Text>
+        </Box>
+    <Box>
+            <Heading as="h3" size="md">
+              Can I use this without connecting my bank directly?
+          </Heading>
+          <Text>Yes, we allow direct upload of bank statements if you just want to track your expenses month over month.
+            Some features will not be supported, such as net worth tracking, but the transaction visualization, reports and budgeting will still work.
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading as="h3" size="md">
+            Is the app open source?
+          </Heading>
+          <Text>
+            Yes, the app is open source (
                       <Link to="https://github.com/mmc102/yearly_report" target="_blank" style={{ textDecoration: "underline", color: "blue" }}>
               here
             </Link>
@@ -50,12 +62,7 @@ export function FAQPage() {
           </Text>
         </Box>
 
-        <Box>
-          <Heading as="h3" size="md">
-            How do you parse bank statements without configuration?
-          </Heading>
-          <Text>LLMs.</Text>
-        </Box>
+        
       </VStack>
     </Box>
   );
