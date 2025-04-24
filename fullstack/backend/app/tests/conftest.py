1e649c75a072216d6902be4ddc5621fa2f013565
Matt Carroll <mcarroll1220@gmail.com>
2025-04-24 10:47:38 +0200

    restore some working tests

--- fullstack/backend/app/tests/conftest.py
+++ fullstack/backend/app/tests/conftest.py
@@ -2,33 +2,22 @@ from collections.abc import Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app.core.db import engine, init_db
 from app.main import app
-from app.models import User
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session",autouse=True)
 def db() -> Generator[Session, None, None]:
     # Connect to the database and begin a transaction
-    connection = engine.connect()
-    transaction = connection.begin()
+    with Session(engine) as session:
+        init_db(session)
 
-    # Create a session bound to the connection
-    session = Session(bind=connection)
-
-    try:
         yield session
-    finally:
-        # Roll back the transaction after the test completes
-        session.close()
-        transaction.rollback()
-        connection.close()
 
 
 @pytest.fixture(scope="module")
